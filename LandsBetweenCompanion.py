f692eceff2c6fceb04f1f92978da5c3b3a473add
Brandon Tran <btran565@gmail.com>
2022-05-03 19:59:33 -0700

    commit on Step-1-Testing

--- LandsBetweenCompanion.py
+++ LandsBetweenCompanion.py
@@ -1,25 +1,51 @@
-#import other files here
 
-def main():
-    
-    #todo: input type error checking
-    print("Welcome to the Lands Between Companion!\nEnter your Elden Ring character's level:")
-    playerLevel = input()
+def attributes():
+
+    inputs = []
+    print("Welcome to the Lands Between Companion! Enter your Elden Ring character's level:")
+    inputs.append(input())
     print("Enter your character's vigor:")
-    playerVigor = input()
+    inputs.append(input())
     print("Enter your character's mind:")
-    playerMind = input()
+    inputs.append(input())
     print("Enter your character's endurance:")
-    playerEndurance = input()
+    inputs.append(input())
     print("Enter your character's strength:")
-    playerStrength = input()
+    inputs.append(input())
     print("Enter your character's dexterity:")
-    playerDexterity = input()
+    inputs.append(input())
     print("Enter your character's intelligence:")
-    playerIntelligence = input()
+    inputs.append(input())
     print("Enter your character's faith:")
-    playerFaith = input()
+    inputs.append(input())
     print("Enter your character's arcane:")
-    playerArcane = input()
+    inputs.append(input())
+
+    return inputs
+
+def main():     
+    #todo: input type error checking
+    #user inputs character stats
 
     
+    while True:
+        attList = attributes()
+
+        print("Player's attributes:\n")
+        print("Level: " +attList[0])
+        print("Vigor: " +attList[1])
+        print("Mind: " +attList[2])
+        print("Endurance: " +attList[3])
+        print("Strength: " +attList[4])
+        print("Dexterity: "+attList[5])
+        print("Intelligence: " +attList[6])
+        print("Faith: " +attList[7])
+        print("Arcane: " +attList[8])
+
+        print("\nIs this correct? (yes/no)")
+        answer = input()
+        if answer.lower() == "yes":
+            break
+        
+
+main()
