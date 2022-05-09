6e0b8f197cb0e1b68ee13f90deab66bb51b3d656
Brandon Tran <btran565@gmail.com>
2022-05-09 09:44:05 -0700

    created item.py and __init__ for a new weapon class

---
+++ item.py
@@ -0,0 +1,10 @@
+class weapon():
+    def __init__(self, id, name, image, description, attack, defence, requiredAttributes):
+        self.id = id
+        self.name = name
+        self.image = image
+        self.description = description
+        self.attack = attack
+        self.defence = defence
+        self.requiredAttributes = requiredAttributes
+
