5b06ad563f247c9bd2eacb95ceb05ea45c2f0ca0
Brandon Tran <btran565@gmail.com>
2022-05-09 11:47:24 -0700

    optimized getAttributes and main using dictionaries. getEquipment in progress

--- LandsBetweenCompanion.py
+++ LandsBetweenCompanion.py
@@ -3,37 +3,37 @@ import json
 import item
 
 def getAttributes():
-    inputs = []
+    attList = ['level', 'vigor', 'mind', 'endurance', 'strength', 'dexterity', 'intelligence', 'faith', 'arcane']
+    attDict = dict(zip(attList, [None] * len(attList)))
     print("Welcome to the Lands Between Companion! Enter your Elden Ring character's level:")
-    inputs.append(input())
+    attDict[attList[0]] = input()
     print("Enter your character's vigor:")
-    inputs.append(input())
+    attDict[attList[1]] = input()
     print("Enter your character's mind:")
-    inputs.append(input())
+    attDict[attList[2]] = input()
     print("Enter your character's endurance:")
-    inputs.append(input())
+    attDict[attList[3]] = input()
     print("Enter your character's strength:")
-    inputs.append(input())
+    attDict[attList[4]] = input()
     print("Enter your character's dexterity:")
-    inputs.append(input())
+    attDict[attList[5]] = input()
     print("Enter your character's intelligence:")
-    inputs.append(input())
+    attDict[attList[6]] = input()
     print("Enter your character's faith:")
-    inputs.append(input())
+    attDict[attList[7]] = input()
     print("Enter your character's arcane:")
-    inputs.append(input())
+    attDict[attList[8]] = input()
 
-    return inputs
+    return attDict
 
 def getEquipment():
     response = requests.get("https://eldenring.fanapis.com/api/weapons?limit=400")
     json_data = json.loads(response.text)
     items = makeDict(json_data)
-    left, right = 0
     print("Enter the number of weapons equipped in the right hand: ")
-    right.append(input())
+    right = input()
     print("Enter the number of weapons equipped in the left hand: ")
-    left.append(input())
+    left = input()
 
 
     return
@@ -49,18 +49,11 @@ def main():
     #todo: input type error checking
     #user inputs character stats
     while True:
-        attList = getAttributes()
-
+        attDict = getAttributes()
         print("Player's attributes:\n")
-        print("Level: " +attList[0])
-        print("Vigor: " +attList[1])
-        print("Mind: " +attList[2])
-        print("Endurance: " +attList[3])
-        print("Strength: " +attList[4])
-        print("Dexterity: "+attList[5])
-        print("Intelligence: " +attList[6])
-        print("Faith: " +attList[7])
-        print("Arcane: " +attList[8])
+        for k, v in attDict.items():
+            print(k.capitalize()+': '+v)
+
 
         print("\nIs this correct? (yes/no)")
         answer = input()
