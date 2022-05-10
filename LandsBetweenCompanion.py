9daa71b81504e48d43180e81ca1d23acd49912f5
Brandon Tran <btran565@gmail.com>
2022-05-09 17:01:17 -0700

    renamed functions using lowercase and underscores to adhere to python naming convention

--- LandsBetweenCompanion.py
+++ LandsBetweenCompanion.py
@@ -4,16 +4,17 @@ import item
 import algorithms
 
 
-def getAttributes():
+def get_attributes():
     attList = ['level', 'vigor', 'mind', 'endurance', 'strength', 'dexterity', 'intelligence', 'faith', 'arcane']
-    attDict = dict(zip(attList, [None] * len(attList)))
+    attDict = dict(zip(attList, [None] * len(attList)))  # creates dict with attributes as keys and None's as values
     print("Welcome to the Lands Between Companion!\n")
     for i in range(len(attList)):
         print("Enter your character's " + attList[i] + ":")
         attDict[attList[i]] = input()
     return attDict
 
-def getEquipment():     ##needs error checking for right/left ints
+
+def get_equipment():  # needs error checking for right/left ints
     print("Enter the number of items equipped in the right hand: ")
     rightNum = int(input())
     print("Enter the number of items equipped in the left hand: ")
@@ -22,13 +23,14 @@ def getEquipment():     ##needs error checking for right/left ints
     rightHand = []
     if rightNum > 0:
         for i in range(rightNum):
-            print("Right hand equipment #" + str(i+1) + ":")
+            print("Right hand equipment #" + str(i + 1) + ":")
             print("Is this equipped item a Weapon or Shield?\n")
             itemCate = input().lower()
-            itemDict = getAPI(itemCate)
+            itemDict = get_api(itemCate)
             if itemCate == 'weapon':
                 print("Please select the weapon's category:")
-                print("Daggers, Straight Swords, Greatswords, Colossal Swords,\nThrusting Swords, Heavy Thrusting Swords, Curved Swords, Curved Greatswords,\nKatanas, Twinblades, Axes, Greataxes,\nHammers, Flails, Great Hammers, Colossal Weapons,\nSpears, Great Spears, Halberds, Reapers,\nWhips, Fists, Claws, Light Bows,\nBows, Greatbows, Crossbows, Ballistae,\nGlintstone Staffs, Sacred Seals, Torches")
+                print(
+                    "Daggers, Straight Swords, Greatswords, Colossal Swords,\nThrusting Swords, Heavy Thrusting Swords, Curved Swords, Curved Greatswords,\nKatanas, Twinblades, Axes, Greataxes,\nHammers, Flails, Great Hammers, Colossal Weapons,\nSpears, Great Spears, Halberds, Reapers,\nWhips, Fists, Claws, Light Bows,\nBows, Greatbows, Crossbows, Ballistae,\nGlintstone Staffs, Sacred Seals, Torches")
                 algorithms.getCategoryList(itemDict, input().lower())
             if itemCate == 'shield':
                 print("Please select the shield's category:(small, medium, great)")
@@ -38,26 +40,33 @@ def getEquipment():     ##needs error checking for right/left ints
 
     return
 
-def getAPI(category):    #returns dict of specified category in API   ##need to add error checking for 'category'
+
+def get_api(category):  # returns dict of specified category in API   ##need to add error checking for 'category'
     json_data = {}
-    for i in range(4):      #reads multiple pages of API to fill local json library
+    for i in range(4):  # reads multiple pages of API to fill local json library
         response = requests.get("https://eldenring.fanapis.com/api/" + category + "s" + "?limit=100&page=" + "0")
         json_data.update(json.loads(response.text))
-    items = makeDict(json_data, category)
+    items = make_dict(json_data, category)
     return items
 
-def makeDict(data, category):  #takes json data and category and makes dict ## add more conditionals to make dicts of other categories
+
+def make_dict(data,
+              category):  # takes json data and category and makes dict ## add more conditionals to make dicts of other categories
     dict = {}
     if (category == "weapon"):
         for i in range(len(data['data'])):
             weapon = item.Weapon(data['data'][i]['id'], data['data'][i]['name'], data['data'][i]['image'],
-                                 data['data'][i]['description'], data['data'][i]['category'], data['data'][i]['weight'], data['data'][i]['attack'], data['data'][i]['defence'], data['data'][i]['requiredAttributes'],
+                                 data['data'][i]['description'], data['data'][i]['category'], data['data'][i]['weight'],
+                                 data['data'][i]['attack'], data['data'][i]['defence'],
+                                 data['data'][i]['requiredAttributes'],
                                  data['data'][i]['scalesWith'])
             dict[data['data'][i]['name']] = weapon
     if (category == "shield"):
         for i in range(len(data['data'])):
             shield = item.Weapon(data['data'][i]['id'], data['data'][i]['name'], data['data'][i]['image'],
-                                 data['data'][i]['description'], data['data'][i]['category'], data['data'][i]['weight'], data['data'][i]['attack'], data['data'][i]['defence'], data['data'][i]['requiredAttributes'],
+                                 data['data'][i]['description'], data['data'][i]['category'], data['data'][i]['weight'],
+                                 data['data'][i]['attack'], data['data'][i]['defence'],
+                                 data['data'][i]['requiredAttributes'],
                                  data['data'][i]['scalesWith'])
             dict[data['data'][i]['name']] = shield
     return dict  # returns dictionary of item names and item objects
@@ -67,7 +76,7 @@ def main():
     # user inputs character stats
     # add input type checking later
     while True:
-        attDict = getAttributes()  # creates dict of attributes
+        attDict = get_attributes()  # creates dict of attributes
         print("Player's attributes:\n")
         for k, v in attDict.items():
             print(k.capitalize() + ': ' + v)
@@ -76,7 +85,7 @@ def main():
         answer = input()
         if answer.lower() == "yes":
             break
-    equipList = getEquipment()
+    equipList = get_equipment()
 
 
 main()
