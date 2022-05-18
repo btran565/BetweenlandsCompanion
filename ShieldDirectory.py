2a6666b103a3a49c3b86e82236cce627f221ba68
D238252 <cdavis91@horizon.csueastbay.edu>
2022-05-18 16:39:43 -0700

    Prelim of Directory

--- ShieldDirectory.py
+++ ShieldDirectory.py
@@ -7,6 +7,18 @@ fRead = fileSType.read()
 sTypes = fRead.split('\n')
 print(sTypes)
 
+fRead = fileRS.read()
+sList = [x.split(',') for x in fRead.split('\n')]
+
+sOList = [[],[],[]]
+
+for i in sList:
+    i.pop()
+    sOList[sTypes.index(i[1])].append(i[0])
+
+for i in range(3):
+    print(sTypes[i],": ",len(sOList[i])," : ",sOList[i])
+
 
 fileRS.close()
 fileSType.close()
