d0f1469d76e1a9e5d1a58591809bf08a9dac4401
D238252 <cdavis91@horizon.csueastbay.edu>
2022-05-18 15:59:26 -0700

    Started prelim for Directory for weapons, will adjust later into functions to be used

--- WeaponDirectory.py
+++ WeaponDirectory.py
@@ -4,11 +4,33 @@
 # Note: I absolutely hate the PEP8 reminders, half require me to unlearn proper protocol in
 # every other language
 
+# from Weapn import Weapn
+
+
 fileWType = open('info files\\WeaponTypes.txt','r')
+fileRW = open('info files\\Weapons.txt','r')
 
 fRead = fileWType.read()
 wTypes = fRead.split('\n')
-
 print(wTypes)
 
+fRead = fileRW.read()
+
+wList = [x.split(',') for x in fRead.split('\n')]
+wList.pop()
+
+count = 0
+
+for i in wList:
+    i.pop()
+
+wOList = [[] for i in range(len(wTypes))]
+
+for i in wList:
+    wOList[wTypes.index(i[1])].append(i[0])
+
+for i in range(len(wOList)):
+    print(wTypes[i]," : ",len(wOList[i])," : ",wOList[i])
+
+fileRW.close()
 fileWType.close()
